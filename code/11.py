76d0a6f1fa179806b043cc1ea3b88ad626064c12
csmaterial <csmaterial@users.noreply.github.com>
2016-10-22 20:56:51 +0800

    Create 11.py

---
+++ code/11.py
@@ -0,0 +1,9 @@
+print "how old are you?",#','make the print and age in the same line
+age = raw_input()
+print"what is your name",
+name = raw_input()
+print "how tall are you?",
+tall = raw_input()
+
+print "so, you're %r old,name is %r,and %r heavy"%(
+    age,name,tall)
