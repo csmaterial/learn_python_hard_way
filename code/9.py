8fac914f95b77ac935b1d95fc13716427976785d
csmaterial <csmaterial@users.noreply.github.com>
2016-10-22 10:59:55 +0800

    Create 9.py

---
+++ code/9.py
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\n"
+
+print "here are days",days
+print days
+print "here are months"
+print months
+print """
+There are
+something going on here
+with the three double quotes
+we will be able to type as much as we like
+even 5lines if we want
+"""
