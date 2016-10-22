92e8e7121ab10ecaa81e4f0afcd0d83b1c721a10
csmaterial <csmaterial@users.noreply.github.com>
2016-10-22 11:11:17 +0800

    Create 10.py

---
+++ code/10.py
@@ -0,0 +1,15 @@
+
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "Il'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do lilst:
+\t* cat food
+\t* fishies
+\t*catnip\n\t* grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
